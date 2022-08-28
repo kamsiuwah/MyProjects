a062302c9cd5f3a8e7decd703b53c8e5acc9f3c1
kamsiuwah <kuwah@mcneese.edu>
2022-08-28 16:37:52 -0500

    first commit

---
+++ Tree.java
@@ -0,0 +1,14 @@
+// Define your tree class in this file!
+public class Tree{
+  public static void main(String[] args){
+    // Introduce myself and print a tree to the screen
+    System.out.println("Hello there, I'm Kamsi and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("*******");
+    System.out.println("  *** ");
+    System.out.println(" ***** ");
+    System.out.println("   *  ");
+    System.out.println("   *  ");
+    System.out.println("~~~~~~~~");
+  }
+}
